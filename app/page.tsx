8f09eb504840432d54a5bef566a6ecf09384540b
s3xmaxman <badside3919@gmail.com>
2024-08-02 05:00:32 +0900

    delete 32 files and update 1 file

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,18 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
-      <h1 className="text-white text-3xl underline">Home</h1>
-      <Button>Click me</Button>
+    <div className="flex h-screen max-h-screen">
+      <section className="remove-scrollbar container my-auto">
+        <div className="sub-container max-w-[496px]">
+          <Image
+            src="/assets/icons/logo-full.svg"
+            height={1000}
+            width={1000}
+            alt="patient"
+            className="mb-12 h-10 w-fit"
+          />
+        </div>
+      </section>
     </div>
   );
 }
