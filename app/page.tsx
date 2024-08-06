837608c214ef4accb9507d856d23266106c55984
s3xmaxman <badside3919@gmail.com>
2024-08-07 03:42:51 +0900

    update page.tsx and PassKeyModal.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 import PatientForm from "@/components/forms/PatientForm";
 import PassKeyModal from "@/components/PassKeyModal";
-import { Button } from "@/components/ui/button";
 import Image from "next/image";
 import Link from "next/link";
 
--- components/PassKeyModal.tsx
+++ components/PassKeyModal.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import {
   AlertDialog,
   AlertDialogAction,
@@ -11,12 +11,53 @@ import {
   AlertDialogTitle,
   AlertDialogTrigger,
 } from "@/components/ui/alert-dialog";
+import {
+  InputOTP,
+  InputOTPGroup,
+  InputOTPSeparator,
+  InputOTPSlot,
+} from "@/components/ui/input-otp";
+
 import Image from "next/image";
-import { useRouter } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
+import { decryptKey, encryptKey } from "@/lib/utils";
 
 const PassKeyModal = () => {
   const router = useRouter();
-  const [open, setOpen] = useState(false);
+  const path = usePathname();
+  const [open, setOpen] = useState(true);
+  const [passkey, setPasskey] = useState("");
+  const [error, setError] = useState("");
+
+  const encryptedKey =
+    typeof window !== "undefined" ? localStorage.getItem("passkey") : null;
+
+  useEffect(() => {
+    const accessKey = encryptedKey && decryptKey(encryptedKey);
+
+    if (path)
+      if (accessKey === process.env.NEXT_PUBLIC_ADMIN_PASSKEY!.toString()) {
+        setOpen(false);
+        router.push("/admin");
+      } else {
+        setOpen(true);
+      }
+  }, [encryptedKey]);
+
+  const validatePasskey = (
+    e: React.MouseEvent<HTMLInputElement, MouseEvent>
+  ) => {
+    e.preventDefault();
+
+    if (passkey === process.env.NEXT_PUBLIC_ADMIN_PASSKEY) {
+      const encryptedKey = encryptKey(passkey);
+
+      localStorage.setItem("passkey", encryptedKey);
+      setOpen(false);
+    } else {
+      setError("パスキーが正しくありません。");
+    }
+  };
 
   const closeModal = () => {
     setOpen(false);
@@ -28,7 +69,7 @@ const PassKeyModal = () => {
       <AlertDialogContent className="shad-alert-dialog">
         <AlertDialogHeader>
           <AlertDialogTitle className="flex items-center justify-between">
-            Admin Access Verification
+            管理者アクセス検証
             <Image
               src="/assets/icons/close.svg"
               alt="close"
@@ -39,13 +80,37 @@ const PassKeyModal = () => {
             />
           </AlertDialogTitle>
           <AlertDialogDescription>
-            To access the admin page, please enter the passkey.
+            管理者ページにアクセスするには、パスキーを入力してください。
           </AlertDialogDescription>
         </AlertDialogHeader>
-        <div></div>
+        <div>
+          <InputOTP
+            maxLength={6}
+            value={passkey}
+            onChange={(value) => setPasskey(value)}
+          >
+            <InputOTPGroup className="shad-otp">
+              <InputOTPSlot className="shad-otp-slot" index={0} />
+              <InputOTPSlot className="shad-otp-slot" index={1} />
+              <InputOTPSlot className="shad-otp-slot" index={2} />
+              <InputOTPSlot className="shad-otp-slot" index={3} />
+              <InputOTPSlot className="shad-otp-slot" index={4} />
+              <InputOTPSlot className="shad-otp-slot" index={5} />
+            </InputOTPGroup>
+          </InputOTP>
+          {error && (
+            <div className="shad-error text-14-regular mt-4 flex justify-center">
+              {error}
+            </div>
+          )}
+        </div>
         <AlertDialogFooter>
-          <AlertDialogCancel>Cancel</AlertDialogCancel>
-          <AlertDialogAction>Continue</AlertDialogAction>
+          <AlertDialogAction
+            onClick={(e) => validatePasskey(e)}
+            className="shad-primary-btn w-full"
+          >
+            検証
+          </AlertDialogAction>
         </AlertDialogFooter>
       </AlertDialogContent>
     </AlertDialog>
