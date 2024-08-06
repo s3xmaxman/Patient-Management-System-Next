433d03fb0508b275d05b2a7222fe180b03b8ab94
s3xmaxman <badside3919@gmail.com>
2024-08-07 02:09:25 +0900

    update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,8 @@ import { Button } from "@/components/ui/button";
 import Image from "next/image";
 import Link from "next/link";
 
-export default function Home() {
+export default function Home({ searchParams }: SearchParamProps) {
+  const isAdmin = searchParams?.admin === "true";
   return (
     <div className="flex h-screen max-h-screen">
       <section className="remove-scrollbar container my-auto">
