5c909afea6735b4f2c188cca780e24236aa2a8e2
s3xmaxman <badside3919@gmail.com>
2024-08-07 02:43:50 +0900

    build(deps): update page.tsx, package-lock.json and package.json

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import PatientForm from "@/components/forms/PatientForm";
+import PassKeyModal from "@/components/PassKeyModal";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
 import Link from "next/link";
@@ -7,6 +8,7 @@ export default function Home({ searchParams }: SearchParamProps) {
   const isAdmin = searchParams?.admin === "true";
   return (
     <div className="flex h-screen max-h-screen">
+      {isAdmin && <PassKeyModal />}
       <section className="remove-scrollbar container my-auto">
         <div className="sub-container max-w-[496px]">
           <Image
